9c87c63980eac310f52e80dd2a5f049f87cd8773
star-plucking <1328638004@qq.com>
2025-11-12 22:40:08 +0800

    配置 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  // 如果仓库名是 dreamchaser-web，base 应该是 '/dreamchaser-web/'
+  // 如果部署到自定义域名，base 应该是 '/'
+  base: process.env.NODE_ENV === 'production' ? '/dreamchaser-web/' : '/',
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
